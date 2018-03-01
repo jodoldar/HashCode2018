709891803eecc898dfa39c85d61bba952b9904a6
DESKTOP-U3BVR13\Carlos <charliroiggarcia12@gmail.com>
2018-03-01 19:23:17 +0100

    Matriz
    Matriz inicial cogiendo los dos primeros enteros del fichero.

--- HashCode2018/src/HashCode18/Main.java
+++ HashCode2018/src/HashCode18/Main.java
@@ -12,5 +12,20 @@ package HashCode18;
 public class Main {
     public static void main(String[] args){
         
+        System.out.println("HashCode2017");
+        if(args.length!=1){
+            System.err.println("Numero de argumentos incorrecto");
+            System.exit(1);
+        }else{
+        	File f = new File(args[0]);
+            Scanner in = new Scanner(f);
+            int R,C;
+            R = f.nextInt();
+            C = f.nextInt(); 
+            int grid [] [] = new int [R] [C]; 
+            
+        }
+
+
     }
 }
