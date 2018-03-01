e8bd904ac24fe6da67f2bf1aab2d8650ad124259
DESKTOP-U3BVR13\Carlos <charliroiggarcia12@gmail.com>
2018-03-01 19:30:18 +0100

    Fix Matrix
    Matriz arreglada

--- HashCode2018/src/HashCode18/Main.java
+++ HashCode2018/src/HashCode18/Main.java
@@ -4,24 +4,27 @@
  * and open the template in the editor.
  */
 package HashCode18;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  *
  * @author josep
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
         
         System.out.println("HashCode2017");
         if(args.length!=1){
             System.err.println("Numero de argumentos incorrecto");
             System.exit(1);
         }else{
-        	File f = new File(args[0]);
+            File f = new File(args[0]);
             Scanner in = new Scanner(f);
             int R,C;
-            R = f.nextInt();
-            C = f.nextInt(); 
+            R = in.nextInt();
+            C = in.nextInt(); 
             int grid [] [] = new int [R] [C]; 
             
         }
